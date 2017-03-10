0342feee122010d5832146486b4fc1a3642cd58b
Drodriguez819 <Drodriguez819@student.alamo.edu>
2017-03-10 00:06:07 -0600

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,4 +1,5 @@
 public class Person {
+
     private String strFirstName = "";
     private String strLastName = "";
     private String strMiddleName = "";
@@ -9,36 +10,108 @@ public class Person {
     private int intHeightIN = 72;
     private int intWeightLBS = 165;
 
-    public void setFirstName(String pstrFirstName) {
-        this.strFirstName = pstrFirstName;
-    }
-    public String getFirstName() {
-        return this.strFirstName;
-    }
+    //setter and getter for first name
+                public void setFirstName(String pstrFirstName) {
+                    this.strFirstName = pstrFirstName;
+                }
+                public String getFirstName() {
+                    return this.strFirstName;
+                }
+    //setter and getter for middle name
+                public void setLastName(String pstrLastName) {
+                    this.strLastName = pstrLastName;
+                }
+                public String getLastName() {
+                    return this.strLastName;
+                }
+    //Setter and getter for middle name
+                public void setMiddleName(String pstrMiddleName) {
+                    this.strMiddleName = pstrMiddleName;
+                }
+                public String getMiddleName() {
+                    return this.strMiddleName;
+                }
+    //setter and getter for first middle and last name (all in one bs)
+                public String getName() {
+                    return this.getFirstName() + " " + this.getMiddleName() + " " + this.getLastName();
+                }
+   
+    //uses setters and getters to change the bs uptop for name
+        public void setName(String pstrFirstName, 
+                                String pstrMiddleName,
+                                String pstrLastName) {
+            this.setFirstName(pstrFirstName);
+            this.setMiddleName(pstrMiddleName);
+            this.setLastName(pstrLastName);
+                 }
 
-    public void setLastName(String pstrLastName) {
-        this.strLastName = pstrLastName;
-    }
-    public String getLastName() {
-        return this.strLastName;
-    }
+    //setter and getter for birthyear
+                public int setBirthYear(int pintBirthYear) {
+                    this.intBirthYear = pintBirthYear;
+                }
+                public int getBirthYear(){
+                    return this.intBirthYear;
+                }
+    //birth month setter and getter 
+                public int setBirthMonth(int pintBirthMonth){
+                    this.intBirthMonth = pintBirthMonth;
+                }
+                public int getBirthMonth (){
+                    return this.intBirthMonth;
+                }
+    //birthday setter and getter 
+                public int setBirthDay(int pintBirthDay){
+                    this.intBirthDay = pintBirthDay;
+                }
+                public int getBirthDay(){
+                    return this.intBirthDay;
+                }
+    //Birthday bs all in one
+                public int getDayofBirth(){
+                    return this.getBirthMonth() + "/"+ this.getBirthDay + "/" + this.getBirthYear;
+                }
+    //uses setters and getters to get the birthday of individual
+        public void setDayofBirth(int pintBirthYear,
+                                  int pintBirthDay,
+                                  int pintBirthMonth){
+            this.setBirthDay(pintBirthDay);
+            this.setBirthMonth(pintBirthMonth)
+            this.setBirthYear(pintBirthYear);
+                }
 
-    public void setMiddleName(String pstrMiddleName) {
-        this.strMiddleName = pstrMiddleName;
-    }
-    public String getMiddleName() {
-        return this.strMiddleName;
-    }
+//setters and getters for gender and characteristics of individual
+        
+    //gender
+                public char setchrGender(char pchrGender){
+                    this.chrGender = pchrGender;
+                }
+                public char getchrGender(){
+                    return this.chrGender;
+                }
+    //intHeightIN setter and getters 
+                public int setintHeightIN(int pintHeightIN){
+                    this.intHeightIN = pintHeightIN;
+                }
+                public int getintHeightIN(){
+                    return this.intHeightIN;
+                }
+    //intWeightLBS setter and getter
+                public int setintWeightLBS(int pintWeightLBS){
+                    this.intWeightLBS = pintWeightLBS;
+                }
+                public int getWeightLBS(){
 
-    public String getName() {
-        return this.getFirstName() + " " + this.getMiddleName() + " " + this.getLastName();
-    }
-
-    public void setName(String pstrFirstName, 
-                            String pstrMiddleName,
-                            String pstrLastName) {
-        this.setFirstName(pstrFirstName);
-        this.setMiddleName(pstrMiddleName);
-        this.setLastName(pstrLastName);
-    }
+                }
+    //bs for characteristics all together
+        public int getcharacteristics(){
+            return this.getWeightLBS + "," + this.getintHeightIN;
+            }
+    public void getallcharacteristics(char pchrGender,
+                                      int  pintHeightIN,
+                                      int  pintWeightLBS){
+            this.(setchrGender);
+            this.(setintHeightIN);
+            this.(setintWeightLBS);
+         }
+        
 }
