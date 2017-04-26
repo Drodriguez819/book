4d8a7f0638e8cf8d0a2c97c3c4f7ecb1bb5e2a0b
Drodriguez819 <Drodriguez819@users.noreply.github.com>
2017-04-25 20:24:18 -0500

    Add files via upload
    Updated Person class and fixed getters and setters.

--- AddressBook.java
+++ AddressBook.java
@@ -1,14 +1,18 @@
 public class AddressBook {
 
     public static void main(String [] strArgs) {
+
         Person objPersonOne = new Person();
-        System.out.println(objPersonOne.getName());
-        objPersonOne.setFirstName("Chris");
-        objPersonOne.setMiddleName("R");
-        objPersonOne.setLastName("MacDougald");
+
         System.out.println(objPersonOne.getName());
 
-        objPersonOne.setName("J","F","K");
+            objPersonOne.setFirstName("Chris");
+            objPersonOne.setMiddleName("R");
+            objPersonOne.setLastName("MacDougald");
+
+        System.out.println(objPersonOne.getName());
+            objPersonOne.setName("John","F","Kennedy");
+
         System.out.println(objPersonOne.getName());
     }
 }
