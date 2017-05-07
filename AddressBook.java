b676bf715bda84c60f4b04b0eba0dce6e33e1230
Drodriguez819 <Drodriguez819@users.noreply.github.com>
2017-05-07 16:43:57 -0500

    Update AddressBook.java

--- AddressBook.java
+++ AddressBook.java
@@ -1,11 +1,27 @@
+/*
+* Name: David Rodriguez
+* Section: COSC/ITSE 1336
+* Homework: Excercise 11
+* Description: Displays names from person class
+*/
+
+/*
+* -------------------------------PSUDO CODE---------------------------------------------
+*  1. Calls Person class 
+*  2. Displays Person class in a string 
+*   
+*---------------------------------------------------------------------------------------
+*/
 public class AddressBook {
+
+
 
     public static void main(String [] strArgs) {
         Person objPersonOne = new Person();
         System.out.println(objPersonOne.getName());
-        objPersonOne.setFirstName("Chris");
+        objPersonOne.setFirstName("John");
         objPersonOne.setMiddleName("R");
-        objPersonOne.setLastName("MacDougald");
+        objPersonOne.setLastName("Legend");
         System.out.println(objPersonOne.getName());
 
         objPersonOne.setName("J","F","K");
